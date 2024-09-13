f64ecd430cbb39d2dd9fcc55ddd2805bddfefda4
maitreverge <verge.f@gmail.com>
2024-09-13 09:23:34 +0200

    working on `nginx` Dockerfile

--- srcs/requirements/nginx/conf/zsh/basic_zsh.sh
+++ srcs/requirements/nginx/conf/zsh/basic_zsh.sh
@@ -2,3 +2,4 @@ echo 'export ZSH="$HOME/.oh-my-zsh"' > ~/.zshrc
 echo 'ZSH_THEME="robbyrussell"' >> ~/.zshrc
 echo 'plugins=()' >> ~/.zshrc
 echo 'source $ZSH/oh-my-zsh.sh' >> ~/.zshrc
+export URL=flverge.42.fr #local env variable for nginx container only
