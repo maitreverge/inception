91b1f0fdb34d445f60793bdadfb65a80d72cb1a3
de_la_verge <verge.f@gmail.com>
2024-09-18 10:00:32 +0200

    Delete nginx local variables stuff

--- srcs/requirements/nginx/conf/nginx/swap_env_nginx.sh
+++
@@ -1,8 +0,0 @@
-#/bin/sh
-
-# This command take the pre nginx config file
-# and return the same version with the local env variable swapped
-/bin/sh -c " envsubst < /etc/nginx/pre_nginx.conf > /etc/nginx/nginx.conf"
-
-# Delete the old config file
-rm -rf /etc/nginx/pre_nginx.conf
