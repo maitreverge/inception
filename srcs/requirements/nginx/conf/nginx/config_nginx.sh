c5ae4a538f6ef0e4affaadfec1f861e7faa7a64c
de_la_verge <verge.f@gmail.com>
2024-09-18 10:06:40 +0200

    Refactored `RUN` instructions in a script

---
+++ srcs/requirements/nginx/conf/nginx/config_nginx.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+
+# Create the html directory for stocking the base html file
+mkdir -p /var/www/html
+
+# ? IS USELESS
+# Building nginx configuration path
+mkdir -p /var/run/nginx
+
+# Give authorization on the root
+chmod 755 /var/www/html
+
+# Create the data user and adds it to the www-data group
+adduser -S -G www-data www-data
+
+# Give authorization on the main user
+chown -R www-data:www-data /var/www/html
+
+# Clean useless packages for optimizing container size
+apk cache clean
