9380c234cc49bd056079b0d934cc97d2b3814d03
maitreverge <verge.f@gmail.com>
2024-09-20 13:56:25 +0200

    sleep 5 for mysql to start

--- srcs/requirements/mariadb/conf/mariadb_init_script.sh
+++ srcs/requirements/mariadb/conf/mariadb_init_script.sh
@@ -9,15 +9,10 @@ set -e
 
 # if [ ! -d "/path/to/directory" ] checks if a directory does not exists
 
+
 # Checks if the path /run/mysqld exists
 # be responsible for storing temporary files and important files
 # for running MariaDB, such as the PID file and connection socket in the Dockerfile for MariaDB
-if [ ! -d "/run/mysqld" ]; then
-    echo "/run/mysqld does not exists, creating..."
-    mkdir -p "/run/mysqld"
-    echo "/run/mysqld CREATED"
-    chown root:root /run/mysqld
-fi
 
 # Install Mariadb if, by any chance, shit's has not been installed
 if [ ! -d "/var/lib/mysql/mysql" ]; then
@@ -37,23 +32,25 @@ fi
 # Running the MariaDB service in background
 mysqld_safe &
 
-# Create the database if such does not exists
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${MARIADB_DATABASE_NAME}\`;"
+sleep 5
+
+# Creatbe the database if such does not exists
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MARIADB_DATABASE_NAME}\`;"
 
 # Init the main user with his password
-mysql -e "CREATE USER IF NOT EXISTS \`${MARIADB_USER_LOGIN}\`@'localhost' IDENTIFIED BY '${MARIADB_USER_PASSWORD}';"
+mariadb -e "CREATE USER IF NOT EXISTS \`${MARIADB_USER_LOGIN}\`@'localhost' IDENTIFIED BY '${MARIADB_USER_PASSWORD}';"
 
 # Grant full access to the database to the created user.
-mysql -e "GRANT ALL PRIVILEGES ON \`${MARIADB_DATABASE_NAME}\`.* TO \`${MARIADB_USER_LOGIN}\`@'%' IDENTIFIED BY '${MARIADB_USER_PASSWORD}';"
+mariadb -e "GRANT ALL PRIVILEGES ON \`${MARIADB_DATABASE_NAME}\`.* TO \`${MARIADB_USER_LOGIN}\`@'%' IDENTIFIED BY '${MARIADB_USER_PASSWORD}';"
 
 # Modify root user
-mysql -e "ALTER USER '${MARIADB_ROOT_LOGIN}'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';"
+mariadb -e "ALTER USER '${MARIADB_ROOT_LOGIN}'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';"
 
 # Refresh MariaDB to apply the aboves changes
-mysql -e "FLUSH PRIVILEGES;"
+mariadb -e "FLUSH PRIVILEGES;"
 
 # Restart MariaDB
 # ! NOTE :   (no need to ${VARIABLE} because this is a regular shell command)
 mysqladmin -u root -p $MARIADB_ROOT_PASSWORD shutdown
 
-mysqld_safe
+# mariadb-safe
