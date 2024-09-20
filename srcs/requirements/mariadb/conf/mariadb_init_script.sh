9426c297134121aa9e2590ca969ee59101729068
de_la_verge <verge.f@gmail.com>
2024-09-20 09:53:55 +0200

    Mariadb work scripts

--- srcs/requirements/mariadb/conf/mariadb_init_script.sh
+++ srcs/requirements/mariadb/conf/mariadb_init_script.sh
@@ -16,14 +16,19 @@ if [ ! -d "/run/mysqld" ]; then
     chown root:root /run/mysqld
 fi
 
+# Install Mariadb if, by any chance, shit's has not been installed
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-    # echo "/run/mysqld does not exists, creating..."
-    # mkdir -p "/run/mysqld"
-    # echo "/run/mysqld CREATED"
-    # chown root:root /run/mysqld
+    echo "MariaDB, for an obscure yet unknown reason, does not exists..."
+    echo "Init MariaDB....."
     mysql_install_db
-    echo "mariadb est pas installe securite o max"
+    echo "MariaDB is Created, time to DROP DATABSE; boys"
 fi
 
+# Create the database if such does not exists
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${MARIADB_DATABASE_NAME}\`;"
 
+# Init the main user with his password
+mysql -e "CREATE USER IF NOT EXISTS \`${MARIADB_MAIN_USER}\`@'localhost' IDENTIFIED BY '${MARIADB_MAIN_PASSWORD}';"
 
+# Grant full access to the database to the created user.
+mysql -e "GRANT ALL PRIVILEGES ON \`${MARIADB_DATABASE_NAME}\`.* TO \`${MARIADB_MAIN_USER}\`@'%' IDENTIFIED BY '${MARIADB_MAIN_PASSWORD}';"
