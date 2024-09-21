d3711f88e51f10f0e8397cebb8b9af02c806fbae
maitreverge <verge.f@gmail.com>
2024-09-21 14:53:29 +0200

    Fixed nginx

--- srcs/requirements/nginx/conf/nginx/config_nginx.sh
+++ srcs/requirements/nginx/conf/nginx/config_nginx.sh
@@ -3,6 +3,15 @@
 # If a command fails, script will stop immediately
 set -e
 
+#Create a config directory
+mkdir -p /var/run/nginx
+
+# Give authorization on the root
+chmod 755 /var/www/html
+
+# Give authorization on the main user
+chown -R www-data:www-data /var/www/html
+
 # Build Path for certificate
 mkdir -p $PATH_NGINX_CERTIFICATE
 
@@ -10,29 +19,11 @@ mkdir -p $PATH_NGINX_CERTIFICATE
 mkdir -p $PATH_NGINX_TSL_KEY
 
 # Creating the self-signed certificate
-openssl req -x509 -nodes -out $PATH_NGINX_CERTIFICATE/inception.crt	-keyout $PATH_NGINX_TSL_KEY/inception.key -subj "/C=FR/ST=IDF/L=Paris/O=42/OU=42/CN=$URL/UID=$LOGIN"
-
-# Create the html directory for stocking the base html file
-mkdir -p /var/www/html
-
-# Move the index.html in the correct directory after copying it from the Dockerfile
-mv /var/index.html /var/www/html/
-
-# ? IS USELESS
-# Building nginx configuration path
-mkdir -p /var/run/nginx
-
-# Give authorization on the root
-chmod 755 /var/www/html
-
-# # Create the data user and adds it to the www-data group
-# adduser -S -G www-data www-data
-
-# Give authorization on the main user
-chown -R www-data:www-data /var/www/html
-
-# Clean useless packages for optimizing container size
-apt clean
+openssl req -x509 \
+            -nodes \
+            -out $PATH_NGINX_CERTIFICATE/inception.crt \
+            -keyout $PATH_NGINX_TSL_KEY/inception.key \
+            -subj "/C=FR/ST=IDF/L=Paris/O=42/OU=42/CN=flverge.42.fr/UID=flverge"
 
 # Runs the nginx process as a non daemon process,
 # to avoid PID 1 to rip it off, become a zombie process and then shut-down the container.
