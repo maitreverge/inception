4e50dcb474e8ff9ecd2b932fd1aac53b9eab7785
de_la_verge <verge.f@gmail.com>
2024-09-21 12:11:26 +0200

    WP core config done

--- srcs/requirements/wordpress/scripts/wp-config.sh
+++ srcs/requirements/wordpress/scripts/wp-config.sh
@@ -17,16 +17,16 @@ if [ ! -f /var/www/wordpress/wp-config.php ]; then
                         --dbuser=${MARIADB_USER_LOGIN} \
                         --dbpass=${MARIADB_USER_PASSWORD} \
                         --dbhost=mariadb:3306 \
-                        --path='/var/www/wordpress'
+                        --path='/var/www/wordpress' \
     
     # Create the admin login details
     wp core install     --allow-root \
                         --url=${URL} \
-                        --title=${WEBSITE_TITLE}
-                        --admin_user=${WP_ADMIN_LOGIN}
-                        --admin_password=${WP_ADMIN_PASSWORD}
-                        --admin_email=
-                        --skip-email=
+                        --title=${WEBSITE_TITLE} \
+                        --admin_user=${WP_ADMIN_LOGIN} \
+                        --admin_password=${WP_ADMIN_PASSWORD} \
+                        --admin_email${WP_ADMIN_EMAIL} \
+                        --skip-email \
                         --path='/var/www/html'
 
     # Create another user which is not root for Wordpress
