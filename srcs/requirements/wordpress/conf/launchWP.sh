41bd7b9c97a644f53a583259c3071c567925e7c7
“maitreverge_macbook_AUS” <verge.f@gmail.com>
2024-12-20 10:38:34 +0100

    wip wordpress config

--- srcs/requirements/wordpress/conf/launchWP.sh
+++ srcs/requirements/wordpress/conf/launchWP.sh
@@ -16,5 +16,24 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 sudo mv wp-cli.phar /usr/local/bin/wp
 
+# Change the config sample into the real config
+mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+
+##################### CHANGE CONFIG IN WORDPRESS CONFIG ########################
+
+# /** The name of the database for WordPress */
+# define( 'DB_NAME', 'database_name_here' );
+sed -i -r "s/database_name_here/$MARIADB_DATABASE_NAME/1"   wp-config.php
+
+# /** Database username */
+# define( 'DB_USER', 'username_here' );
+sed -i -r "s/username_here/$MARIADB_USER_LOGIN/1"  wp-config.php
 
 
+# /** Database password */
+# define( 'DB_PASSWORD', 'password_here' );
+sed -i -r "s/password_here/$MARIADB_USER_PASSWORD/1"    wp-config.php
+
+# /** Database hostname */
+# define( 'DB_HOST', 'localhost' );
+sed -i -r "s/localhost/mariadb/1"    wp-config.php  #(to connect with mariadb database)
