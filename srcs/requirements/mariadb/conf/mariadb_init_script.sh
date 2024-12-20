9c8481de1c15a6eeb5cc5ef10ce356e1c0b51d9b
“maitreverge_macbook_AUS” <verge.f@gmail.com>
2024-12-20 13:14:36 +0100

    wip mariadb

--- srcs/requirements/mariadb/conf/mariadb_init_script.sh
+++ srcs/requirements/mariadb/conf/mariadb_init_script.sh
@@ -2,3 +2,14 @@
 
 # Change the bind address from `127.0.0.1`` to `0.0.0.0`
 sed -i -r "s/127.0.0.1/0.0.0.0/1"    /etc/mysql/mariadb.conf.d/50-server.cnf
+
+
+echo "CREATE DATABASE IF NOT EXISTS $MARIADB_DATABASE_NAME ;" > db1.sql
+echo "CREATE USER IF NOT EXISTS '$MARIADB_USER_LOGIN'@'%' IDENTIFIED BY '$MARIADB_USER_PASSWORD' ;" >> db1.sql
+echo "GRANT ALL PRIVILEGES ON $MARIADB_DATABASE_NAME.* TO '$MARIADB_USER_LOGIN'@'%' ;" >> db1.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
+echo "FLUSH PRIVILEGES;" >> db1.sql
+
+mysql < db1.sql
+
+rm db1.sql
