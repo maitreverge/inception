19a0481b3421a1495bfc80c33000912b0df201a5
“maitreverge_macbook_AUS” <verge.f@gmail.com>
2024-12-21 14:42:41 +0100

    Fixing quotes in MYSQL script

--- srcs/requirements/mariadb/conf/mariadb_init_script.sh
+++ srcs/requirements/mariadb/conf/mariadb_init_script.sh
@@ -1,19 +1,30 @@
 #! /bin/sh
 
-# Change the bind address from `127.0.0.1`` to `0.0.0.0`
-sed -i -r "s/127.0.0.1/0.0.0.0/1"    /etc/mysql/mariadb.conf.d/50-server.cnf
+# Allows the script to exit if any of the commands fails
+set -e
 
-service mysql start
+# Launching mysql as a background task
+mysqld_safe &
 
+echo "Creating database: ${MARIADB_DATABASE_NAME} ..."
 sleep 10
 
-echo "CREATE DATABASE IF NOT EXISTS $MARIADB_DATABASE_NAME ;" > db1.sql
-echo "CREATE USER IF NOT EXISTS '$MARIADB_USER_LOGIN'@'%' IDENTIFIED BY '$MARIADB_USER_PASSWORD' ;" >> db1.sql
-echo "GRANT ALL PRIVILEGES ON $MARIADB_DATABASE_NAME.* TO '$MARIADB_USER_LOGIN'@'%' ;" >> db1.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
+echo "CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';" >> db1.sql # DONE
+echo "ALTER USER 'root'@'%' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';" >> db1.sql # DONE
+echo "CREATE DATABASE IF NOT EXISTS \`${MARIADB_DATABASE_NAME}\`;" >> db1.sql # DONE
+echo "CREATE USER IF NOT EXISTS \`${MARIADB_USER_LOGIN}\`@'%' IDENTIFIED BY '${MARIADB_USER_PASSWORD}';" >> db1.sql
+echo "GRANT ALL PRIVILEGES ON \`${MARIADB_DATABASE_NAME}\`.* TO \`${MARIADB_USER_LOGIN}\`@'%';" >> db1.sql
+echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;" >> db1.sql
 echo "FLUSH PRIVILEGES;" >> db1.sql
 
 mysql < db1.sql
+
+# If last_exit_status == 0
+if [ $? -eq 0 ]; then
+    echo "Database ${MARIADB_DATABASE_NAME} created successfully."
+else
+    echo "Failed to create database."
+fi
 
 rm db1.sql
 
