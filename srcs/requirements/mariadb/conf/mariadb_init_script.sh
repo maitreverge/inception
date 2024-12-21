07157efe578ab58d9a5fe55aa615518f70ad5ed6
“maitreverge_macbook_AUS” <verge.f@gmail.com>
2024-12-21 17:28:57 +0100

    MariaDB stabilized

--- srcs/requirements/mariadb/conf/mariadb_init_script.sh
+++ srcs/requirements/mariadb/conf/mariadb_init_script.sh
@@ -3,29 +3,45 @@
 # Allows the script to exit if any of the commands fails
 set -e
 
-# Launching mysql as a background task
-mysqld_safe &
+# Validate ENV variables before going further
+if [ -z "$MARIADB_DATABASE_NAME" ] || [ -z "$MARIADB_ROOT_PASSWORD" ] || [ -z "$MARIADB_USER_LOGIN" ] || [ -z "$MARIADB_USER_PASSWORD" ]; then
+    echo "One or more required environment variables are missing:"
+    echo "MARIADB_DATABASE_NAME, MARIADB_ROOT_PASSWORD, MARIADB_USER_LOGIN, MARIADB_USER_PASSWORD"
+    exit 1
+fi
 
-echo "Creating database: ${MARIADB_DATABASE_NAME} ..."
+# Launching mysql as a background task
+mysqld_safe --skip-networking &
+
+echo "Starting MariaDB daemon process: $MARIADB_DATABASE_NAME ..."
 sleep 10
 
-echo "CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';" >> db1.sql # DONE
-echo "ALTER USER 'root'@'%' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';" >> db1.sql # DONE
-echo "CREATE DATABASE IF NOT EXISTS \`${MARIADB_DATABASE_NAME}\`;" >> db1.sql # DONE
-echo "CREATE USER IF NOT EXISTS '${MARIADB_USER_LOGIN}'@'%' IDENTIFIED BY '${MARIADB_USER_PASSWORD}';" >> db1.sql
-echo "GRANT ALL PRIVILEGES ON \`${MARIADB_DATABASE_NAME}\`.* TO \`${MARIADB_USER_LOGIN}\`@'%';" >> db1.sql
-echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;" >> db1.sql
-echo "FLUSH PRIVILEGES;" >> db1.sql
+echo "CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';" > init_db.sql # DONE
+echo "ALTER USER 'root'@'%' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';" >> init_db.sql # DONE
+echo "CREATE DATABASE IF NOT EXISTS \`${MARIADB_DATABASE_NAME}\`;" >> init_db.sql # DONE
+echo "CREATE USER IF NOT EXISTS '${MARIADB_USER_LOGIN}'@'%' IDENTIFIED BY '${MARIADB_USER_PASSWORD}';" >> init_db.sql # DONE
+echo "GRANT ALL PRIVILEGES ON \`${MARIADB_DATABASE_NAME}\`.* TO \`${MARIADB_USER_LOGIN}\`@'%';" >> init_db.sql # DONE
+echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;" >> init_db.sql # DONE
+echo "FLUSH PRIVILEGES;" >> init_db.sql
 
-mysql < db1.sql
+echo "Initializing database..."
 
-# If last_exit_status == 0
+mariadb < init_db.sql
+
+# Checks if the database init went well
 if [ $? -eq 0 ]; then
     echo "Database ${MARIADB_DATABASE_NAME} created successfully."
 else
     echo "Failed to create database."
+	exit 1
 fi
 
-rm db1.sql
+rm init_db.sql
 
-# bash /usr/bin/mysqld_safe
+# Kill current instance of mysqld_safe to avoid getting two instances of mysql running in the same container
+killall mysqld_safe
+wait
+
+# Start MariaDB in the foreground to keep the container alive
+# The exec command replaces the shell process (or the current script) with the mysqld process
+exec mysqld
