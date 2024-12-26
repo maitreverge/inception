e624bb349104df36e00f8765f0db07bff9428fb1
“maitreverge_macbook_AUS” <verge.f@gmail.com>
2024-12-26 20:28:29 +0100

    wip ftp

--- srcs/requirements/bonus/2_ftp/conf/launch_FTP.sh
+++ srcs/requirements/bonus/2_ftp/conf/launch_FTP.sh
@@ -1,6 +1,28 @@
 #! /bin/sh
 
-#Enabling vsftpd
-systemctl start vsftpd
-systemctl enable vsftpd
+# Quit if fails
+set -e
 
+#Enabling vsftpd
+# service vsftpd start
+
+# Add specific user
+useradd -m -s /bin/bash $FTP_USER
+
+echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
+
+# Add user to vsftpd user list
+echo "$FTP_USER" >> /etc/vsftpd.userlist
+
+# FTP User now have full permission over the Wordpress to write files
+chown -R $FTP_USER:$FTP_USER /var/www/html
+
+
+# /usr/sbin/vsftpd
+
+systemctl start vsftpd
+
+bash /usr/sbin/vsftpd /etc/vsftpd.conf
+
+
+# tail -f /dev/null
